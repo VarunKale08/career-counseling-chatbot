b46cd0f849bde69ac278451ad1cf102431094080
Varun <varun08kale@gmail.com>
2023-10-13 21:08:07 +0530

    Created an svm model connected it with azure and the flask code

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
 from flask import Flask, render_template, request, jsonify
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVC
+import joblib
 
 app = Flask(__name__)
+
+# Load the trained SVM model
+svm_model = joblib.load('career_counseling_model.joblib')
+
+# Function to preprocess the data
+def preprocess_data(df):
+    df.columns = df.columns.str.strip()
+    scaler = MinMaxScaler()
+    numerical_columns = ['Mathematics', 'Science', 'Social_Studies', 'English']
+    df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
+    return df
+
+# Function to get career suggestion
+def get_career_suggestion(user_input):
+    user_input = preprocess_data(user_input)
+    prediction = svm_model.predict(user_input)
+    return prediction[0]
 
 @app.route('/')
 def index():
@@ -12,6 +33,12 @@ def get_bot_response():
     # Add your bot logic here to process user_text and get bot's response
     # For now, I'll just echo back the user's input
     return jsonify({'response': user_text})
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    user_input = request.json  # Assuming it contains user's input
+    interested_field = get_career_suggestion(user_input)
+    return jsonify({'interested_field': interested_field})
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ svm_code.py
@@ -0,0 +1,57 @@
+# Step 2: Prepare Data
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+# Read the CSV Data
+df = pd.read_csv('/content/Sheet1.csv')
+
+# Drop any duplicate rows, if any
+df = df.drop_duplicates()
+
+# Remove leading and trailing spaces from column names
+df.columns = df.columns.str.strip()
+
+# Check for missing values
+print(df.isna().sum())
+
+# Output:
+# Name                 0
+# Mathematics          0
+# Science              0
+# Social_Studies       0
+# English              0
+# Optional_subject     0
+# Interested_field     0
+# dtype: int64
+
+# There are no missing values, so no further handling is needed.
+
+# Scale Numerical Values (Mathematics, Science, Social_Studies, English)
+scaler = MinMaxScaler()
+numerical_columns = ['Mathematics', 'Science', 'Social_Studies', 'English']
+df[numerical_columns] = scaler.fit_transform(df[numerical_columns])
+
+# Inspect the Preprocessed Data
+print(df.head())
+
+# Step 3: Train and Save the Model
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+import joblib
+
+# Split Data into Training and Testing Sets
+X = df[['Mathematics', 'Science', 'Social_Studies', 'English']]  # Features
+y = df['Interested_field']  # Target variable
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Initialize and Train the SVM Model
+svm_model = SVC(kernel='linear')
+svm_model.fit(X_train, y_train)
+
+# Evaluate Model Performance
+accuracy = svm_model.score(X_test, y_test)
+print(f'Accuracy: {accuracy * 100:.2f}%')
+
+# Save the Trained Model
+joblib.dump(svm_model, 'career_counseling_model.joblib')
